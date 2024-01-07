5364c1445a08558d21531af7c004ce7bae337a10
Muhammad Popoola <popoolama@yahoo.com>
2024-01-07 21:27:20 +0100

    create a job, listings and error 404 views

--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -1,7 +1,6 @@
 <?php
 loadPartials('head');
 loadPartials('navbar');
-loadPartials('showcase-search');
 loadPartials('top-banner');
 
 ?>
@@ -9,7 +8,7 @@ loadPartials('top-banner');
 <!-- Job Listings -->
 <section>
     <div class="container mx-auto p-4 mt-4">
-        <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Recent Jobs</div>
+        <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">All Jobs</div>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
             <!-- Job Listing 1: Software Engineer -->
             <div class="rounded-lg shadow-md bg-white">
@@ -141,6 +140,7 @@ loadPartials('top-banner');
                 </div>
             </div>
 
+            <!-- Job Listing 6: Data Scientist -->
             <div class="rounded-lg shadow-md bg-white">
                 <div class="p-4">
                     <h2 class="text-xl font-semibold">Data Scientist</h2>
@@ -166,10 +166,6 @@ loadPartials('top-banner');
                 </div>
             </div>
         </div>
-        <a href="/listings" class="block text-xl text-center">
-            <i class="fa fa-arrow-alt-circle-right"></i>
-            Show All Jobs
-        </a>
 </section>
 
 <?= loadPartials('bottom-banner') ?>
