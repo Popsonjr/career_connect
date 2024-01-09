fa6670a22ed5401ef01ac43a587da882164c2387
Muhammad Popoola <popoolama@yahoo.com>
2024-01-09 21:48:08 +0100

    refactor router to class

--- router.php
+++ router.php
@@ -1,9 +1,48 @@
 <?php
-$routes = require basePath('routes.php');
+class Router {
+    protected $routes = [];
 
-if (array_key_exists($uri, $routes)) {
-    require(basePath($routes[$uri]));
-} else {
-    http_response_code(404);
-    require basePath($routes['404']);
+    /**
+     * Add a GET route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function get($uri, $controller) {
+        
+    }
+
+    /**
+     * Add a POST route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function post($uri, $controller) {
+        
+    }
+
+    /**
+     * Add a PUT route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function PUT($uri, $controller) {
+        
+    }
+
+    /**
+     * Add a DELETE route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function delete($uri, $controller) {
+        
+    }
 }
