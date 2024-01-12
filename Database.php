a3b85d5f6d4cdaa536850b15c026861f049d589c
Muhammad Popoola <popoolama@yahoo.com>
2024-01-12 09:57:29 +0100

    fetch function

--- Database.php
+++ Database.php
@@ -21,4 +21,22 @@ class Database {
             throw new Exception("Database connection failed: {$e->getMessage()}");
         }
     }
+
+    /**
+     * Query the database
+     * 
+     * @param string $query
+     * 
+     * @return PDOStatement
+     * @throws PDOException
+     */
+    public function query($query) {
+        try{
+            $statement = $this->conn->prepare($query);
+            $statement->execute();
+            return $statement;
+        } catch(PDOException $e) {
+            throw new Exception("Query failed to execute: {$e->getMessage()}");
+        }
+    }
 }
