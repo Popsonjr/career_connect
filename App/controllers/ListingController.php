8bab4a7e8a9406ace185fc48f9422c387cf4b947
Muhammad Popoola <popoolama@yahoo.com>
2024-01-21 01:04:11 +0100

    flash messages

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -151,6 +151,9 @@ class ListingController
 
         $this->db->query('DELETE FROM listings WHERE id = :id', $params);
 
+        //set flash message
+        $_SESSION['success_message'] = 'Listing deleted successfully';
+
         redirect('/listings');
     }
 }
