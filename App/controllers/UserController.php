cbd4004a5bc097ab4502b38d049975a0dbf53833
Muhammad Popoola <popoolama@yahoo.com>
2024-01-23 09:24:14 +0100

    register functionality

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -80,6 +80,39 @@ class UserController
             ]);
             exit;
         }
-        inspectAndDie('store');
+
+        // check if emails exists
+        $params = [
+            'email' => $email
+        ];
+
+        $user = $this->db->query("SELECT * FROM users WHERE email = :email", $params)->fetch();
+
+        if($user) {
+            $errors['email'] = "Email already exists";
+            loadView('users/create', [
+                'errors' => $errors,
+                'user' => [
+                    'name' => $name,
+                    'email' => $email,
+                    'city' => $city,
+                    'state' => $state,
+                ]
+            ]);
+            exit;
+        }
+
+        // create user account
+        $params = [
+            'name' => $name,
+            'email' => $email,
+            'city' => $city,
+            'state' => $state,
+            'password' => password_hash($password, PASSWORD_DEFAULT)
+        ];
+
+        $this->db->query('INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)', $params);
+        
+        redirect('/');
     }
 }
